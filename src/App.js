9d41cf19a911af8b12605ac8f43ab3e882bea29d
CarlosHurand <Carlos_hurand@hotmail.com>
2022-04-04 21:49:23 -0500

    Class activity nr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { PersistentDrawerLeft } from "./Components/Pages";
 
-function App() {
+function App() 
+{
+  //return (
+    //<div className="App">
+      //<header className = "App-header">
+        //<img src = {logo} className = "App-logo" alt = "logo" />
+        //<p>
+          //Edit <code>src/App.js</code> Hola Juan Carlos.
+        //</p>
+        //<a
+          //className="App-link"
+          //href="https://reactjs.org"
+          //target="_blank"
+          //rel = "noopener noreferrer"
+        //>
+          //Learn React
+        //</a>
+      //</header>
+    //</div>
+  //);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Hola Juan Carlos.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <Router>
+    //   <Link to = "/PersistentDrawerLeft"> PersistentDrawerLeft </Link>
+    //   <hr />
+    //   <Routes>
+    //     <Route path = "/title" element = {<PersistentDrawerLeft />} />
+    //   </Routes>
+    // </Router>
+
+    <div className = "App">
+      <PersistentDrawerLeft />
     </div>
   );
 }
-
 export default App;
