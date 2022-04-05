2d98d34821b8589e3bc723a86c93c8388c59bc97
CarlosHurand <Carlos_hurand@hotmail.com>
2022-04-04 21:53:49 -0500

    Added routers

--- src/App.js
+++ src/App.js
@@ -4,38 +4,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { PersistentDrawerLeft } from "./Components/Pages";
 
-function App() 
-{
-  //return (
-    //<div className="App">
-      //<header className = "App-header">
-        //<img src = {logo} className = "App-logo" alt = "logo" />
-        //<p>
-          //Edit <code>src/App.js</code> Hola Juan Carlos.
-        //</p>
-        //<a
-          //className="App-link"
-          //href="https://reactjs.org"
-          //target="_blank"
-          //rel = "noopener noreferrer"
-        //>
-          //Learn React
-        //</a>
-      //</header>
-    //</div>
-  //);
+function App() {
   return (
-    // <Router>
-    //   <Link to = "/PersistentDrawerLeft"> PersistentDrawerLeft </Link>
-    //   <hr />
-    //   <Routes>
-    //     <Route path = "/title" element = {<PersistentDrawerLeft />} />
-    //   </Routes>
-    // </Router>
-
-    <div className = "App">
-      <PersistentDrawerLeft />
-    </div>
+    <Router>
+      <Link to="/Title"> Title </Link>
+      <hr />
+      <Routes>
+        <Route path="/Title" element={<PersistentDrawerLeft />} />
+      </Routes>
+    </Router>
   );
 }
 export default App;
